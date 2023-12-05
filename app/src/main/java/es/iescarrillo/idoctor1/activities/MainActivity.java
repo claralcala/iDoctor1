cb5a4397aa0372d33475020f0d36b96eabaaf65f
Clara Alcalá García <calcgar312@iescarrillo.es>
2023-12-05 14:04:51 +0100

    clases_persona

--- app/src/main/java/es/iescarrillo/idoctor1/activities/MainActivity.java
+++ app/src/main/java/es/iescarrillo/idoctor1/activities/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import es.iescarrillo.idoctor1.R;
+import es.iescarrillo.idoctor1.models.Professional;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
