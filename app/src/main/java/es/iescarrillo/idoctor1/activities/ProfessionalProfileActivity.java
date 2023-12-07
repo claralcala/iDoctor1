0409e75dfd2e22bbd92b0427854f19f246a39174
Clara Alcalá García <calcgar312@iescarrillo.es>
2023-12-07 12:14:33 +0100

    actividades

---
+++ app/src/main/java/es/iescarrillo/idoctor1/activities/ProfessionalProfileActivity.java
@@ -0,0 +1,16 @@
+package es.iescarrillo.idoctor1.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.iescarrillo.idoctor1.R;
+
+public class ProfessionalProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professional_profile);
+    }
+}
