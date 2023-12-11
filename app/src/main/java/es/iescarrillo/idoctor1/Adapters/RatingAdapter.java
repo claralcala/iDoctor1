fee296fd8e29babcb34838db5d0c50caf8999c72
DamianCarrilloSalcedo <dlozmor139@iescarrillo.es>
2023-12-12 00:24:49 +0100

    Cracion interfaces

--- app/src/main/java/es/iescarrillo/idoctor1/Adapters/RatingAdapter.java
+++
@@ -1,9 +0,0 @@
-package es.iescarrillo.idoctor1.Adapters;
-
-import android.widget.ArrayAdapter;
-
-import es.iescarrillo.idoctor1.models.Professional;
-
-public class RatingAdapter extends ArrayAdapter<Professional> {
-
-}
