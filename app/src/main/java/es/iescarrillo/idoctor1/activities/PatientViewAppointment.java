38ce78b4346db60444a18b07f16e04181d4520ee
DamianCarrilloSalcedo <dlozmor139@iescarrillo.es>
2023-12-17 08:08:40 +0100

    Cambios

---
+++ app/src/main/java/es/iescarrillo/idoctor1/activities/PatientViewAppointment.java
@@ -0,0 +1,16 @@
+package es.iescarrillo.idoctor1.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.iescarrillo.idoctor1.R;
+
+public class PatientViewAppointment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_view_appointment);
+    }
+}
