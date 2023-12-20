4590217ed536da70194ebb21114ff4d753f22dee
DamianCarrilloSalcedo <dlozmor139@iescarrillo.es>
2023-12-20 20:05:12 +0100

    Cambios

--- app/src/main/java/es/iescarrillo/idoctor1/activities/PatientViewAssessment.java
+++ app/src/main/java/es/iescarrillo/idoctor1/activities/PatientViewAssessment.java
@@ -2,16 +2,36 @@ package es.iescarrillo.idoctor1.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import es.iescarrillo.idoctor1.R;
 
 public class PatientViewAssessment extends AppCompatActivity {
-
+    ListView
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_view_assessment);
+        //Variables de sesión
+        SharedPreferences sharedPreferences= getSharedPreferences("PreferencesDoctor", Context.MODE_PRIVATE);
+        String username= sharedPreferences.getString("user", "");
+        String role = sharedPreferences.getString("role", "");
+        Boolean login = sharedPreferences.getBoolean("login", true);
+        String id_ = sharedPreferences.getString("id", "");
+        if(!role.equals("PATIENT")){
+            sharedPreferences.edit().clear().apply();
+            Intent backMain = new Intent(this, MainActivity.class);
+            startActivity(backMain);
+        }
+        DatabaseReference dbAssessmentProfessional= FirebaseDatabase.getInstance().getReference().child("appointment");
 
-    }
+        }
 }
